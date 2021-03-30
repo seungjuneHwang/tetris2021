3368690e19ba0d5235b94b396933feb6bc005175
hsj <seungjune.hwang@gmail.com>
2021-03-30 16:41:59 +0900

    반복 추가 리스트 공부

---
+++ test.py
@@ -0,0 +1,24 @@
+for i in range(10):
+    print(i)
+    # print("hello world")
+    # print("자몽")
+
+# ㄹ 블록
+# ㄱ 블록
+# ㄴ 블록
+# ㅁ 블록
+# ㅣ 블록
+# ㅜ 블록
+block = ["ㄹ", "ㄱ", "ㄴ", "ㅁ", "ㅣ", "ㅜ", "q"]
+print(block)
+print("블록 갯수:", len(block))
+for i in range(len(block)):
+    print(block[i])
+# print(block[0])
+# print(block[1])
+# print(block[2])
+# print(block[3])
+# print(block[4])
+# for x in block:
+#     print(x)
+
